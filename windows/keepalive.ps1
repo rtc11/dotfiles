3fbafcd20f50b4f506719f1c0d7de8d622b795ca
Robin T <robin.tordly@gmail.com>
2019-12-10 12:09:33 +0100

    Create keepalive.ps1

---
+++ windows/keepalive.ps1
@@ -0,0 +1,10 @@
+Clear-Host
+Echo "Keep-alive with Scroll Lock..."
+$WShell = New-Object -com "Wscript.Shell"
+while ($true)
+{
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Milliseconds 100
+  $WShell.sendkeys("{SCROLLLOCK}")
+  Start-Sleep -Seconds 10
+}
